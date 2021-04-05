63daad55a46879d1fa6700d4d77b4a79e2ad899f
BeckenbauerRijiirkd <68623856+BeckenbauerRijiirkd@users.noreply.github.com>
2021-04-05 11:55:40 -0300

    Update Servico.java

--- src/main/java/servicos/sa/domain/Servico.java
+++ src/main/java/servicos/sa/domain/Servico.java
@@ -1,5 +1,43 @@
 package servicos.sa.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Servico {
+	
+	@Id
+	private String nome;
+	private String descricao;
+	private long valor;
+	
+	public Servico() {
+		
+	}
+	
+	public Servico(String nome, String descricao, long valor) {
+		this.nome = nome;
+		this.descricao = descricao;
+		this.valor = valor;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public long getValor() {
+		return valor;
+	}
+	public void setValor(long valor) {
+		this.valor = valor;
+	}
+	
 
 }
